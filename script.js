08908ba74cf264c3994162f21f85b05e38cd4c33
AdorkablyNicole <hello@nicolegeorge.me>
2020-07-21 17:34:13 +0930

    script.js file updated - added DOM method to display time event.

--- script.js
+++ script.js
@@ -1,26 +1,39 @@
-var time = new Date().getHours(); // NATIVE JAVASCRIPT CODE: represents current hour (1-24)
-var messageText;
-var noon = 12;
-var evening = 18; // 6PM
-var wakeupTime = 9; // 9AM
-var lunchTime = 12; // 12PM
-var partyTime = 17; // 5PM
-var napTime = lunchTime + 2; // 2PM
+const time = new Date().getHours(); // NATIVE JAVASCRIPT CODE: represents current hour (1-24)
+const messageText;
+const noon = 12;
+const evening = 18; // 6PM
+const wakeupTime = 9; // 9AM
+const lunchTime = 12; // 12PM
+const partyTime = 17; // 5PM
+const napTime = lunchTime + 2; // 2PM
+const message = document.getElementById('timeEvent');
  
 if (time == partyTime){
     messageText = "IZ PARTEE TIME!!";
-}else if (time == napTime){
+}
+else if (time == napTime)
+{
     messageText = "IZ NAP TIME...";
-}else if (time == lunchTime){
+}
+else if (time == lunchTime)
+{
     messageText = "IZ NOM NOM NOM TIME!!";
-}else if (time == wakeupTime){
+}
+else if (time == wakeupTime)
+{
     messageText = "IZ TIME TO GETTUP.";
-}else if (time < noon){
+}
+else if (time < noon)
+{
     messageText = "Good morning, Mate!";
-}else if (time > evening){
+}
+else if (time > evening)
+{
     messageText = "Good Evening, Mate!";
-}else{
+}
+else
+{
     messageText = "Good afternoon, Mate!";
 }
  
-console.log(messageText);
+message.innerText = messageText;
