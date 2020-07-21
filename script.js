af1ab2d78b2ba72107ef514695041395d80950ca
AdorkablyNicole <hello@nicolegeorge.me>
2020-07-21 17:52:56 +0930

    added the clock function

--- script.js
+++ script.js
@@ -7,20 +7,27 @@ const lunchTime = 12; // 12PM
 const partyTime = 17; // 5PM
 const napTime = lunchTime + 2; // 2PM
 const message = document.getElementById('timeEvent');
- 
+const lolCat = document.getElementById('lolcat');
+const image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/08/wakeUpTime.jpg" ;
+
+
 if (time == partyTime){
+    image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat4.jpg";
     messageText = "IZ PARTEE TIME!!";
 }
 else if (time == napTime)
 {
+    image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat3.jpg";
     messageText = "IZ NAP TIME...";
 }
 else if (time == lunchTime)
 {
+    image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat2.jpg";
     messageText = "IZ NOM NOM NOM TIME!!";
 }
 else if (time == wakeupTime)
 {
+    image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat5.jpg";
     messageText = "IZ TIME TO GETTUP.";
 }
 else if (time < noon)
@@ -37,3 +44,47 @@ else
 }
  
 message.innerText = messageText;
+lolCat.src = image;
+
+
+const showCurrentTime = () => 
+{
+    // display the string on the webpage
+    let clock = document.getElementById('clock');
+    let currentTime = new Date();
+
+    let hours = currentTime.getHours();
+    let minutes = currentTime.getMinutes();
+    let seconds = currentTime.getSeconds();
+    let meridian = "AM";
+
+// set hours
+
+    if (hours >= noon) 
+    {
+        meridian = "PM";
+    }
+    if (hours > noon)
+    {
+        hours = hours -12;
+    }
+
+    // set minutes
+    if (minutes < 10)
+    {
+        minutes = "0" + minutes;
+    }
+
+    // set seconds
+    if (seconds < 10)
+    {
+        seconds = "0" + seconds;
+    }
+
+    // display the time as a sting
+
+    const clockTime = hours = ":" + minutes + ":" + seconds + " " + meridian + "!";
+    clock.innerText = clockTime;
+
+    showCurrentTime();
+};
